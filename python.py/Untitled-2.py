ab8cf349698bbce1f910887ed4914041c65b95c3
muhammadabdulmoiz00000-svg <muhammadabdulmoiz00000@gmail.com>
2026-08-05 12:10:08 -0700

    Update Untitled-2.py

--- python.py/Untitled-2.py
+++ python.py/Untitled-2.py
@@ -1,7 +1,10 @@
-print("       *      ")
-print("      ***     ")
-print("     *****    ")
-print("    *******   ")
-print("   *********  ")
-print("  *********** ")
-print(" *************")
+height = 10
+for i in range(height):
+    spaces = " " * (height - i - 1)
+
+    stars = "*" * (2*i +1)
+    print(spaces + stars)
+
+
+#/first i told the program the height of the tree then i used a for loop to set the range of the height and then i created a variable called spaces that will hold the number of spaces that will be printed before the stars and then i created a variabel  called stars that will hold the number of stars that will be printed and then i printed the spaces and stars together to form a christmas tree #/
+# finaly completed the chhristmas tree 
